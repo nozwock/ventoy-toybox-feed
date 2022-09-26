88e5b33d122dca00847373546dbc19f3e87a3954
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-26 19:49:13 +0530

    refactor: make cli.py use relative imports

--- src/toybox_feed/cli.py
+++ src/toybox_feed/cli.py
@@ -1,12 +1,13 @@
 import json
 from pathlib import Path
 from argparse import ArgumentParser
-from scrapers import distrowatch
+from .scrapers import distrowatch
 import timeit
 
 DEFAULT_JSON = "feed.json"
 
-if __name__ == "__main__":
+
+def run():
     parser = ArgumentParser()
     parser.add_argument(
         "-o", "--output", type=str, default=DEFAULT_JSON, metavar="FILE"
@@ -17,3 +18,7 @@ if __name__ == "__main__":
         start_time = timeit.default_timer()
         json.dump(distrowatch.TorrentArchiveScraper().get, f)
         print(f"Done! took {timeit.default_timer() - start_time}s")
+
+
+if __name__ == "__main__":
+    run()
