5d0ea828d3cd359028d1e60fb4984c0d53f2e85a
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-30 03:09:54 +0530

    feat: add async download files impl

---
+++ src/toybox_feed/utils/async_download.py
@@ -0,0 +1,84 @@
+import asyncio
+import aiohttp
+import aiofiles
+from aiolimiter import AsyncLimiter
+from pathlib import Path
+from typing import Any
+
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64)"
+
+
+def download_many(
+    urls: list[str],
+    dir: str | Path = "",
+    headers: dict = {"User-Agent": USER_AGENT},
+    semaphore: asyncio.Semaphore = None,  # type: ignore
+    limiter: AsyncLimiter = None,  # type: ignore
+    **kwargs: Any,
+) -> None:
+    dir = Path(dir) if isinstance(dir, str) else dir
+    dir.mkdir(exist_ok=True, parents=True)
+    semaphore = asyncio.Semaphore(5) if semaphore is None else semaphore
+    limiter = AsyncLimiter(1, 0.125) if limiter is None else limiter
+
+    async def fetch_file(
+        session: aiohttp.ClientSession,
+        url: str,
+        dir: Path,
+    ) -> None:
+        filename = url.split("/")[-1]
+        await semaphore.acquire()
+        async with limiter:
+            logger.info(f"Begin downloading {url}")
+            async with session.get(url) as response:
+                logger.info(f"Recieved response {response.status}")
+                assert response.status == 200
+                f = await aiofiles.open(dir.joinpath(filename), "wb")
+                await f.write(await response.read())
+                logger.info(f"Finished downloading {url}")
+                await f.close()
+                semaphore.release()
+
+    async def main() -> None:
+        """async file download function."""
+        async with aiohttp.ClientSession(headers=headers, **kwargs) as session:
+            await asyncio.gather(
+                *[fetch_file(session, url, dir) for url in urls]  # type: ignore - always will be Path type
+            )
+
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    ###############
+    # DEBUG TESTS #
+    ###############
+    import json
+    import logging
+    from rich.logging import RichHandler
+    from timeit import default_timer as timer
+    from ..scrapers import distrowatch
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"%(message)s",
+        datefmt="%H:%M:%S",
+        handlers=[RichHandler()],
+    )
+
+    feed = distrowatch.TorrentArchiveScraper().get_feed
+    # with open("feed.json", "r") as f:
+    #     feed = json.load(f)
+
+    urls = []
+    for v in feed.values():
+        urls.append(v[0].get("torrent_url"))
+
+    start_time = timer()
+    download_many(urls, "./torrents-files-async2")
+    logger.info(
+        f"[green]{timer() - start_time}s[/green] [yellow]for {len(urls)} urls[/yellow]",
+        extra={"markup": True},
+    )
