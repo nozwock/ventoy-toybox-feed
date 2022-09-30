6d79501890dd2a9a096642bf1b840b4f66409904
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-30 17:37:30 +0530

    refactor: change return type of download()

--- src/toybox_feed/utils/sync_download.py
+++ src/toybox_feed/utils/sync_download.py
@@ -1,25 +1,26 @@
 import requests
 from pathlib import Path
+from typing import Any
 
 RESPONSE_OK = 200
+Response = int
 
 
-def download(url: str, dir: str | Path = "", **kwargs) -> bool:
+def download(url: str, dir: str | Path = "", **kwargs: Any) -> Response:
     """
-    True on successful download else False.\n
     **kwargs are passed to requests.get()
     """
     dir = Path(dir) if isinstance(dir, str) else dir
     dir.mkdir(exist_ok=True, parents=True)
-    name = url.split("/")[-1]
+    filename = url.split("/")[-1]
 
     response = requests.get(url, **kwargs)
     if response.status_code != RESPONSE_OK:
-        return False
+        return response.status_code
 
-    with open(dir.joinpath(name), "wb") as f:
+    with open(dir.joinpath(filename), "wb") as f:
         f.write(response.content)
-    return True
+    return response.status_code
 
 
 if __name__ == "__main__":
@@ -30,6 +31,7 @@ if __name__ == "__main__":
     import logging
     from rich.logging import RichHandler
     from timeit import default_timer as timer
+    from ..scrapers import distrowatch
 
     logging.basicConfig(
         level=logging.INFO,
@@ -37,20 +39,25 @@ if __name__ == "__main__":
         datefmt="%H:%M:%S",
         handlers=[RichHandler(rich_tracebacks=True)],
     )
-    log = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
     # url = (
     #     "https://www.seoclerk.com/pics/000/923/517/6fe74b36d8230c6581e866362fb435e0.jpg"
     # )
-    # download(url)
 
+    # download(url)
     # with open("feed.json", "r") as f:
     #     feed = json.load(f)
-    # urls = []
-    # for v in feed.values():
-    #     urls.append(v[0]["torrent_url"])
-    # start_time = timer()
-    # for url in urls:
-    #     download(url, "./torrents-files")
-    #     log.info(f"{url}")
-    # log.info(f"took {timer() - start_time}s")
+    feed = distrowatch.TorrentArchiveScraper().get_feed
+
+    urls = []
+    for v in feed.values():
+        urls.append(v[0]["torrent_url"])
+    start_time = timer()
+    for url in urls:
+        download(url, "./torrents-files")
+        logger.info(f"{url}")
+    logger.info(
+        f"[green]{timer() - start_time}s[/green] [yellow]for {len(urls)} urls[/yellow]",
+        extra={"markup": True},
+    )
