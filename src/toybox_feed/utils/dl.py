5fdef5a1273d72c4b4d3a4e73b2bb3e1668f185c
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-30 20:20:24 +0530

    feat: add dl module for downloading files
    to replace async and sync download modules

---
+++ src/toybox_feed/utils/dl.py
@@ -0,0 +1,75 @@
+import asyncio
+from pathlib import Path
+from typing import Any
+
+import aiofiles
+import aiohttp
+import aiolimiter
+import requests
+
+RESPONSE_OK = 200
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64)"
+
+Response = int
+Responses = list[int]
+
+
+def download(url: str, dir: str | Path = "", **kwargs: Any) -> Response:
+    """
+    **kwargs are passed to requests.get()
+    """
+    dir = Path(dir) if isinstance(dir, str) else dir
+    dir.mkdir(exist_ok=True, parents=True)
+    filename = url.split("/")[-1]
+
+    response = requests.get(url, **kwargs)
+    if response.status_code != RESPONSE_OK:
+        return response.status_code
+
+    with open(dir.joinpath(filename), "wb") as f:
+        f.write(response.content)
+    return response.status_code
+
+
+def download_many(
+    urls: list[str],
+    dir: str | Path = "",
+    headers: dict = None,  # type: ignore
+    semaphore: asyncio.Semaphore = None,  # type: ignore
+    limiter: aiolimiter.AsyncLimiter = None,  # type: ignore
+    **kwargs: Any,
+) -> Responses:
+    dir = Path(dir) if isinstance(dir, str) else dir
+    dir.mkdir(exist_ok=True, parents=True)
+    headers = {"User-Agent": USER_AGENT} if headers is None else headers
+    semaphore = asyncio.Semaphore(5) if semaphore is None else semaphore
+    limiter = aiolimiter.AsyncLimiter(1, 0.125) if limiter is None else limiter
+    responses: Responses = []
+
+    async def fetch_file(
+        session: aiohttp.ClientSession,
+        url: str,
+    ) -> None:
+        filename = url.split("/")[-1]
+        await semaphore.acquire()
+        async with limiter:
+            # logger.info(f"Begin downloading {url}")
+            async with session.get(url) as response:
+                # logger.info(f"Received response {response.status}")
+                responses.append(response.status)
+                if response.status == RESPONSE_OK:
+                    f = await aiofiles.open(dir.joinpath(filename), "wb")
+                    await f.write(await response.read())
+                    # logger.info(f"Finished downloading {url}")
+                    await f.close()
+                # else:
+                # logger.error(f"Failed to download {url}")
+                semaphore.release()
+
+    async def main() -> None:
+        """async file download function."""
+        async with aiohttp.ClientSession(headers=headers, **kwargs) as session:
+            await asyncio.gather(*[fetch_file(session, url) for url in urls])
+
+    asyncio.run(main())
+    return responses
