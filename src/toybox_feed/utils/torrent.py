ffa312269e9a08a8139defb86016c36d656ac1ae
nozwock <57829219+nozwock@users.noreply.github.com>
2022-10-01 02:35:33 +0530

    refactor: replace custom magnet impl with existing one

--- src/toybox_feed/utils/torrent.py
+++ src/toybox_feed/utils/torrent.py
@@ -4,14 +4,21 @@ from pathlib import Path
 from torrentool.api import Torrent
 
 
-def get_magnet_link(filepath: str | Path) -> str:
+def get_magnet_link(filepath: str | Path) -> str | None:
     torrent_file = Torrent.from_file(filepath)
-    paramstr = urllib.parse.urlencode(  # type: ignore
-        {
-            "xt": torrent_file.magnet_link.split("?xt=")[-1],
-            "dn": torrent_file.name,
-            "tr": torrent_file.announce_urls,
-            "ws": torrent_file.webseeds,
-        }
-    )
-    return f"magnet:?{paramstr}"
+
+    try:
+        return str(torrent_file.get_magnet(detailed=True))
+    except:
+        return None
+
+    # OLD -> will remove in future commits
+    # paramstr = urllib.parse.urlencode(  # type: ignore
+    #     {
+    #         "xt": torrent_file.magnet_link.split("?xt=")[-1],
+    #         "dn": torrent_file.name,
+    #         "tr": torrent_file.announce_urls,
+    #         "ws": torrent_file.webseeds,
+    #     }
+    # )
+    # return f"magnet:?{paramstr}"
